426c094d808fe59ae39aa6e913c06dbc4271c991
Badboy <constantinradulicea@gmail.com>
2024-04-24 14:48:13 +0300

    updates

--- src/TestQtrSensors.cpp
+++ src/TestQtrSensors.cpp
@@ -1,6 +1,6 @@
 #include <StandardCplusplus.h>
 #include <Arduino.h>
-#include <QTRSensors.h>
+#include <LineSensors.h>
 
 /*
   White offset
@@ -38,67 +38,54 @@
 // 0 before being lost. 5000 means the line is directly under sensor 5 or was
 // last seen by sensor 5 before being lost.
 
-QTRSensors qtr;
+#define TOTAL_LINE_SENSORS 5
+#define LINE_SENSOR_1_PIN A0
+#define LINE_SENSOR_2_PIN A1
+#define LINE_SENSOR_3_PIN A2
+#define LINE_SENSOR_4_PIN A3
+#define LINE_SENSOR_5_PIN A4
 
-const uint8_t SensorCount = 5;
-uint16_t sensorValues[SensorCount];
+LineSensors lineSensors(TOTAL_LINE_SENSORS);
+float sensorsReadings[TOTAL_LINE_SENSORS];
+float linePosition;
+
+float BackgroundColorOnlyCalibrationAvarages[TOTAL_LINE_SENSORS] = {
+  300.0f, 300.0f, 300.0f, 300.0f, 300.0f
+};
+
+float LineColorOlyCalibrationAvarages[TOTAL_LINE_SENSORS] = {
+  70.0f, 70.0f, 70.0f, 70.0f, 70.0f
+};
+
+void readLineSensors(float* sensorsReadings_){
+  sensorsReadings_[0] = analogRead(LINE_SENSOR_1_PIN);
+  sensorsReadings_[1] = analogRead(LINE_SENSOR_2_PIN);
+  sensorsReadings_[2] = analogRead(LINE_SENSOR_3_PIN);
+  sensorsReadings_[3] = analogRead(LINE_SENSOR_4_PIN);
+  sensorsReadings_[4] = analogRead(LINE_SENSOR_5_PIN);
+}
 
 void setup()
 {
-  // configure the sensors
-  qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4}, SensorCount);
-  qtr.setEmitterPin(2);
+  pinMode(LINE_SENSOR_1_PIN, INPUT);
+  pinMode(LINE_SENSOR_2_PIN, INPUT);
+  pinMode(LINE_SENSOR_3_PIN, INPUT);
+  pinMode(LINE_SENSOR_4_PIN, INPUT);
+  pinMode(LINE_SENSOR_5_PIN, INPUT);
 
-  delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+  lineSensors.SetBackgroundColorOnlyCalibrationAvarages(BackgroundColorOnlyCalibrationAvarages);
+  lineSensors.SetLineColorOlyCalibrationAvarages(LineColorOlyCalibrationAvarages);
 
-  // analogRead() takes about 0.1 ms on an AVR.
-  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
-  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-
-  // print the calibration minimum values measured when emitters were on
   Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
+  while (!Serial) {
+    delay(100);
   }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
+  
 }
 
 void loop()
 {
-  // read calibrated sensor values and obtain a measure of the line position
-  // from 0 to 5000 (for a white line, use readLineWhite() instead)
-  uint16_t position = qtr.readLineBlack(sensorValues);
-
-  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
-  // reflectance and 1000 means minimum reflectance, followed by the line
-  // position
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
-  }
-  Serial.println(position);
-
-  delay(250);
+  readLineSensors(sensorsReadings);
+  linePosition = lineSensors.ReadSensors(sensorsReadings);
+  Serial.println(linePosition);
 }
